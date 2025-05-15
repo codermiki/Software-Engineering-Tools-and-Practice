d2bc1a2dbc9907160f22231e1f246737df32a0af
codermiki <mikiastadessemiki@gmail.com>
2025-05-15 08:35:38 +0300

    generated code form student_answer class

---
+++ Chapter-4/student_Ansswer.java
@@ -0,0 +1,14 @@
+package onlineExam;
+
+public class student_Ansswer {
+	public int _answerId;
+	public int _questionId;
+	public String _responseText;
+	private float _marksAwarded;
+	public Result _._;
+	public Question _answer;
+
+	public void save() {
+		throw new UnsupportedOperationException();
+	}
+}
