751fbfed244021602fa5bea3ddef5bbb4092df98
codermiki <mikiastadessemiki@gmail.com>
2025-05-15 08:30:43 +0300

    generated code form student class

---
+++ Chapter-4/student.java
@@ -0,0 +1,37 @@
+package onlineExam;
+
+import java.util.Vector;
+import onlineExam.exam;
+import onlineExam.feedback;
+
+public class student extends user {
+	private String _studentId;
+	public Vector<exam> _taken = new Vector<exam>();
+	public Vector<feedback> _unnamed_feedback_ = new Vector<feedback>();
+	public course _._;
+	public Result _unnamed_Result_;
+
+	public examList viewAvailableExam() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void takeExam(int aExamId) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void viewResult(int aExamId) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void startExam() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void submitExam() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void addFeedback(String aText) {
+		throw new UnsupportedOperationException();
+	}
+}
