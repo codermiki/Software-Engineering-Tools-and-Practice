da423f1f95d0f4863ea4aa960e353e79ea2d8799
codermiki <mikiastadessemiki@gmail.com>
2025-05-15 08:36:00 +0300

    generated code form result class

---
+++ Chapter-4/Result.java
@@ -0,0 +1,19 @@
+package onlineExam;
+
+public class Result {
+	public int _resultId;
+	public int _studentId;
+	public int _examId;
+	private float _score;
+	public student _unnamed_student_;
+	public exam _unnamed_exam_;
+	public student_Ansswer _unnamed_student_Ansswer_10;
+
+	public void generateReport() {
+		throw new UnsupportedOperationException();
+	}
+
+	private void viewResult() {
+		throw new UnsupportedOperationException();
+	}
+}
