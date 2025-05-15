e08cd4eda781798dc1f6f79e01e1bf290b70aa62
codermiki <mikiastadessemiki@gmail.com>
2025-05-15 08:27:19 +0300

    generated code form admin class

---
+++ Chapter-4/admin.java
@@ -0,0 +1,45 @@
+package onlineExam;
+
+public class admin extends user {
+	public instructor _assigns;
+
+	public void mangeUserAccount() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void manageCourse() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void assignInstructor(int aCourseId, int aInstructorId) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void manageInstructor() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void manageStudent() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void addCourse(course aCourse) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void manageExam() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void manageSchedule() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void scheduleExam(Exam aExam) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void viewAllfeedback() {
+		throw new UnsupportedOperationException();
+	}
+}
