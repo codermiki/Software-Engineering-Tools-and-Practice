8fc2872eb3057c0179e9a44e78662a8365e362d5
codermiki <mikiastadessemiki@gmail.com>
2025-05-15 08:36:25 +0300

    generated code form feedback class

---
+++ Chapter-4/feedback.java
@@ -0,0 +1,19 @@
+package onlineExam;
+
+public class feedback {
+	public int _feedbackId;
+	public int _studentId;
+	public int _courseId;
+	public String _message;
+	public date _date;
+	public course _unnamed_course_;
+	public student _unnamed_student_;
+
+	public String addFeedback() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void viewFeedback() {
+		throw new UnsupportedOperationException();
+	}
+}
