098d622cee14e7573240e09d0e0853d1851608ba
codermiki <mikiastadessemiki@gmail.com>
2025-05-15 08:09:34 +0300

    generated code form user class diagram

---
+++ Chapter-4/user.java
@@ -0,0 +1,20 @@
+package onlineExam;
+
+public class user {
+	public int _user_id;
+	protected String _name;
+	protected String _password;
+	public String _email;
+
+	public boolean login() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void logout() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void updatePassword() {
+		throw new UnsupportedOperationException();
+	}
+}
